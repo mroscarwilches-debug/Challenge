75eca44c41ddcfde6f745572f4cb71b779d041a4
Wilches <mr.oscarwilches@gmail.com>
2026-05-15 21:25:50 -0500

    Connect DB with docker instance DB gymdb, add 5 users and verify connection succes!

--- database/init.sql
+++ database/init.sql
@@ -1,10 +1,8 @@
 # Database initialization script for the gym-app
-# if not already exists, creates the 'users' table with the following columns:
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-#ensure no zero or negative value
-    weight_kg DECIMAL(5,2) NOT NULL CHECK (weight_kg > 0),
-#automatically set the creatin time
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE public.users (
+    id bigint NOT NULL,
+    "name" varchar NOT NULL,
+    weight_kg decimal NOT NULL,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT users_pk PRIMARY KEY (id)
 );
