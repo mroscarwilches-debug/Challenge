2c2ff4cd2abe1a3a1d6d42f60436645ecd2ef0af
Wilches <mr.oscarwilches@gmail.com>
2026-05-15 23:48:19 -0500

    Update README.md

--- database/init.sql
+++
@@ -1,9 +0,0 @@
-# Database initialization script for the gym-app
-CREATE TABLE public.users (
-    id bigint NOT NULL,
-    "name" varchar NOT NULL,
-    weight_kg decimal NOT NULL,
-    weight_lb decimal NOT NULL,
-    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT users_pk PRIMARY KEY (id)
-);
