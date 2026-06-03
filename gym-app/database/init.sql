d64f428ca1bfe81e53f9c8e811ae27250a36381d
mroscarwilches-debug <oscarwilches.was@gmail.com>
2026-06-02 22:08:48 -0500

    directory gym-app created, Dockerfile, init.sql and docker compose created, connection DB validated and working

---
+++ gym-app/database/init.sql
@@ -0,0 +1,9 @@
+# Database initialization script for the gym-app
+CREATE TABLE public.users (
+    id bigint NOT NULL,
+    "name" varchar NOT NULL,
+    weight_kg decimal NOT NULL,
+    weight_lb decimal NOT NULL,
+    created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT users_pk PRIMARY KEY (id)
+);
